9c4904cca72d0b799883b5f0f838a8852c80901f
Italo Di Renzo <italodirenzo93@gmail.com>
2022-12-23 18:46:38 -0500

    Load and bind textures

--- resources/shaders/basic.frag
+++ resources/shaders/basic.frag
@@ -1,10 +1,14 @@
 #version 330 core
 
 in vec3 ourColor;
+in vec2 TexCoord;
 
 out vec4 FragColor;
 
+uniform sampler2D uSampler;
+
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    //FragColor = vec4(ourColor, 1.0);
+    FragColor = texture(uSampler, TexCoord) * vec4(ourColor, 1.0);
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -15,7 +15,7 @@
 
 #include "Shader.h"
 
-std::string ReadShaderSourceFromFile(const char* fileName)
+std::string ReadShaderSourceFromFile(const char *fileName)
 {
     std::ifstream ifs(fileName);
     if (!ifs.is_open())
@@ -52,19 +52,6 @@ int main(void)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
         return -1;
 
-    /* Vector Math */
-    glm::vec3 v1(1.0, 2.0, 3.0);
-    glm::vec3 v2(10.0, 11.0, 2.0);
-    glm::vec3 v = v1 + v2;
-    std::cout << "Vector Result: " << v.x << " " << v.y << " " << v.z << std::endl;
-
-    /* Load an Image */
-    int w, h, nrChannels;
-    unsigned char *image = stbi_load("./resources/test_image.png", &w, &h, &nrChannels, STBI_default);
-    if (!image)
-        return -1;
-    std::cout << "Width: " << w << " | Height: " << h << " | Channels: " << nrChannels << std::endl;
-
     /* Initialize ImGui */
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -75,11 +62,18 @@ int main(void)
     ImGui_ImplOpenGL3_Init("#version 330 core");
 
     // VBO
-    static const float vertices[] = {
-        // positions // colors
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
+    float vertices[] = {
+        // positions // colors // texture coords
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+    };
+
+    unsigned int indices[] = {
+        // note that we start from 0
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     unsigned int vboID;
@@ -94,26 +88,66 @@ int main(void)
     glBindVertexArray(vaoID);
     glBindBuffer(GL_ARRAY_BUFFER, vboID);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), NULL);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), NULL);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    // EBO
+    unsigned int eboID;
+    glGenBuffers(1, &eboID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
     Shader program("./resources/shaders/basic.vert", "./resources/shaders/basic.frag");
+
+    glActiveTexture(GL_TEXTURE0);
+
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    /* Load an Image */
+    int w, h, nrChannels;
+    unsigned char *image = stbi_load("./resources/textures/container.jpg", &w, &h, &nrChannels, STBI_default);
+    if (!image)
+        return -1;
+    std::cout << "Width: " << w << " | Height: " << h << " | Channels: " << nrChannels << std::endl;
+
+    if (image)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(image);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* ImGui Frame */
-        //ImGui_ImplOpenGL3_NewFrame();
-        //ImGui_ImplGlfw_NewFrame();
-        //ImGui::NewFrame();
-        //ImGui::ShowDemoWindow();
-        //ImGui::Render();
+        // ImGui_ImplOpenGL3_NewFrame();
+        // ImGui_ImplGlfw_NewFrame();
+        // ImGui::NewFrame();
+        // ImGui::ShowDemoWindow();
+        // ImGui::Render();
 
         /* Render here */
         program.use();
@@ -121,14 +155,17 @@ int main(void)
         glClearColor(0.0f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindTexture(GL_TEXTURE_2D, textureID);
         glBindVertexArray(vaoID);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+        glBindTexture(GL_TEXTURE_2D, 0);
         glBindVertexArray(0);
         glUseProgram(0);
 
         /* ImGui Render */
-        //ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
